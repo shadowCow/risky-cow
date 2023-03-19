aa010a5e1393b806395cc0cee2fda1c104f9b124
shadowCow <shadowCow84@gmail.com>
2023-03-19 14:46:47 -0400

    some tictactoe rules.

--- game_service/src/game/adapters/game_tictactoe.test.ts
+++ game_service/src/game/adapters/game_tictactoe.test.ts
@@ -1,1 +1,179 @@
-describe('tic tac toe rules', () => {})
+import { moveWasInvalid } from '../../games/games'
+import {
+  invalidMove,
+  madeMove,
+  moveEndedGame,
+  tie,
+  unfinished,
+  winner,
+} from '../game'
+import {
+  createGameRulesTicTacToe,
+  outOfTurnMessage,
+  TicTacToeRules,
+  TicTacToeState,
+  tileAlreadyOccupiedMessage,
+} from './game_tictactoe'
+
+describe('tic tac toe rules', () => {
+  const rules = createGameRulesTicTacToe()
+
+  describe('isValidMove', () => {
+    test('a valid move', () => {
+      const isValid = rules.isValidMove(rules.initState(), {
+        tile: 0,
+        owner: '1',
+      })
+
+      expect(isValid).toEqual(true)
+    })
+
+    test('an invalid move', () => {
+      const isValid = rules.isValidMove(anEarlyGame(), { tile: 0, owner: '1' })
+
+      expect(isValid).toEqual(false)
+    })
+  })
+
+  describe('initState', () => {
+    test('returns an empty board with player 1 to play', () => {
+      const state = rules.initState()
+
+      expect(state).toEqual(aNewGame())
+    })
+  })
+
+  describe('onMove', () => {
+    test('a valid move that does not end the game', () => {
+      const state = anEarlyGame()
+
+      const result = rules.onMove(state, { tile: 8, owner: '2' })
+
+      expect(result).toEqual(
+        madeMove({
+          state: {
+            board: [
+              '1',
+              '2',
+              '1',
+              undefined,
+              undefined,
+              undefined,
+              undefined,
+              undefined,
+              '2',
+            ],
+            playerTurn: '2',
+          },
+        }),
+      )
+    })
+
+    test('a valid move that ends the game', () => {
+      const state = aLateGame()
+      const result = rules.onMove(state, { tile: 6, owner: '1' })
+
+      expect(result).toEqual(
+        moveEndedGame({
+          state: aLateGameWon(),
+        }),
+      )
+    })
+
+    test('an invalid move', () => {
+      const state = anEarlyGame()
+      const result = rules.onMove(state, { tile: 0, owner: '2' })
+
+      expect(result).toEqual(
+        invalidMove({ reason: tileAlreadyOccupiedMessage(0) }),
+      )
+    })
+
+    test('a move out of turn', () => {
+      const state = anEarlyGame()
+      const result = rules.onMove(state, { tile: 8, owner: '1' })
+
+      expect(result).toEqual(
+        invalidMove({ reason: outOfTurnMessage('2', '1') }),
+      )
+    })
+  })
+
+  describe('getOutcome', () => {
+    test('an unfinished game', () => {
+      const result = rules.getOutcome(aLateGame())
+      expect(result).toEqual(unfinished())
+    })
+
+    test('a game that ended with a tie', () => {
+      const result = rules.getOutcome(aTiedGame())
+      expect(result).toEqual(tie())
+    })
+
+    test('a game that ended with a winner', () => {
+      const result = rules.getOutcome(aLateGameWon())
+      expect(result).toEqual(winner({ player: '1' }))
+    })
+  })
+})
+
+function anEmptyBoard(): TicTacToeState['board'] {
+  const board = [
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+  ]
+
+  return board
+}
+
+function aNewGame(): TicTacToeState {
+  return {
+    board: anEmptyBoard(),
+    playerTurn: '1',
+  }
+}
+
+function anEarlyGame(): TicTacToeState {
+  return {
+    board: [
+      '1',
+      '2',
+      '1',
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+    ],
+    playerTurn: '2',
+  }
+}
+
+function aLateGame(): TicTacToeState {
+  return {
+    board: ['1', '2', '1', '1', '2', '2', undefined, undefined, undefined],
+    playerTurn: '1',
+  }
+}
+
+function aLateGameWon(): TicTacToeState {
+  return {
+    board: ['1', '2', '1', '1', '2', '2', '1', undefined, undefined],
+    playerTurn: '2',
+  }
+}
+
+function aTiedGame(): TicTacToeState {
+  return {
+    board: ['1', '1', '2', '2', '2', '1', '1', '2', '1'],
+    playerTurn: '2',
+  }
+}
--- game_service/src/game/adapters/game_tictactoe.ts
+++ game_service/src/game/adapters/game_tictactoe.ts
@@ -1,32 +1,154 @@
-import { GameRules, invalidMove, unfinished } from '../game'
+import { assertNever } from '../../fp/pattern_matching'
+import { moveWasInvalid } from '../../games/games'
+import {
+  GameRules,
+  invalidMove,
+  madeMove,
+  moveEndedGame,
+  tie,
+  unfinished,
+  winner,
+} from '../game'
 
-export function createGameRulesTicTacToe(): GameRules<
-  TicTacToeState,
-  TicTacToeMove
-> {
+export type TicTacToeRules = GameRules<TicTacToeState, TicTacToeMove>
+
+export function createGameRulesTicTacToe(): TicTacToeRules {
   return {
     isValidMove(s, m) {
-      return false
+      return m.tile >= 0 && m.tile < 9 && s.board[m.tile] === undefined
     },
     initState() {
-      const state: TicTacToeState = []
-      state.fill(undefined, 0, 9)
+      const board: TicTacToeState['board'] = []
+      board.fill(undefined, 0, 9)
 
-      return state
+      return {
+        board,
+        playerTurn: '1',
+      }
     },
     onMove(s, m) {
-      return invalidMove({ reason: 'not implemented' })
+      if (this.isValidMove(s, m)) {
+        const nextBoard = s.board.map((owner, index) => {
+          if (index === m.tile) {
+            return m.owner
+          } else {
+            return owner
+          }
+        })
+
+        const nextState: TicTacToeState = {
+          board: nextBoard,
+          playerTurn: nextTurn(s.playerTurn),
+        }
+
+        const outcome = this.getOutcome(nextState)
+
+        switch (outcome.kind) {
+          case tie.kind:
+          case winner.kind:
+            return moveEndedGame({ state: nextState })
+          case unfinished.kind:
+            return madeMove({ state: nextState })
+          default:
+            assertNever(outcome)
+        }
+      } else {
+        return invalidMove({ reason: tileAlreadyOccupiedMessage(m.tile) })
+      }
     },
     getOutcome(s) {
-      return unfinished()
+      if (
+        s.board[0] !== undefined &&
+        s.board[0] === s.board[1] &&
+        s.board[1] === s.board[2]
+      ) {
+        // row1 winner
+        return winner({ player: s.board[0] })
+      } else if (
+        s.board[3] !== undefined &&
+        s.board[3] === s.board[4] &&
+        s.board[4] === s.board[5]
+      ) {
+        // row2 winner
+        return winner({ player: s.board[3] })
+      } else if (
+        s.board[6] !== undefined &&
+        s.board[6] === s.board[7] &&
+        s.board[7] === s.board[8]
+      ) {
+        // row3 winner
+        return winner({ player: s.board[6] })
+      } else if (
+        s.board[0] !== undefined &&
+        s.board[0] === s.board[3] &&
+        s.board[3] === s.board[6]
+      ) {
+        // col1 winner
+        return winner({ player: s.board[0] })
+      } else if (
+        s.board[1] !== undefined &&
+        s.board[1] === s.board[4] &&
+        s.board[4] === s.board[7]
+      ) {
+        // col2 winner
+        return winner({ player: s.board[1] })
+      } else if (
+        s.board[2] !== undefined &&
+        s.board[2] === s.board[5] &&
+        s.board[5] === s.board[8]
+      ) {
+        // col3 winner
+        return winner({ player: s.board[2] })
+      } else if (
+        s.board[0] !== undefined &&
+        s.board[0] === s.board[4] &&
+        s.board[4] === s.board[8]
+      ) {
+        // diag1 winner
+        return winner({ player: s.board[0] })
+      } else if (
+        s.board[2] !== undefined &&
+        s.board[2] === s.board[4] &&
+        s.board[4] === s.board[6]
+      ) {
+        // diag2 winner
+        return winner({ player: s.board[2] })
+      } else if (s.board.every((tileState) => tileState !== undefined)) {
+        // tie
+        return tie()
+      } else {
+        return unfinished()
+      }
     },
   }
 }
 
-export type TicTacToeState = Array<TileState>
+export type TicTacToeState = {
+  board: Array<TileState>
+  playerTurn: PlayerMarker
+}
 
-export type TileState = TileOwner | undefined
+export type TileState = PlayerMarker | undefined
 
-export type TileOwner = '1' | '2'
+export type PlayerMarker = '1' | '2'
 
-export type TicTacToeMove = {}
+export type TicTacToeMove = { tile: number; owner: PlayerMarker }
+
+function nextTurn(currentTurn: PlayerMarker): PlayerMarker {
+  if (currentTurn === '1') {
+    return '2'
+  } else {
+    return '1'
+  }
+}
+
+export function outOfTurnMessage(
+  currentTurn: PlayerMarker,
+  mover: PlayerMarker,
+): string {
+  return `Cannot move for player ${mover} on player ${currentTurn} turn.`
+}
+
+export function tileAlreadyOccupiedMessage(tileIndex: number): string {
+  return `Tile ${tileIndex} is already occupied.`
+}
